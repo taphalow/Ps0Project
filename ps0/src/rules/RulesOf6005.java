efa352032deb491f65fa81a6c1800f81fe66fd6c
taphalow <lmoustapha2@gmail.com>
2019-03-14 12:55:36 +0000

    première validation du projet

---
+++ ps0/src/rules/RulesOf6005.java
@@ -0,0 +1,20 @@
+package rules;
+
+
+public class RulesOf6005 {
+     
+      
+    public static boolean mayUseCodeInAssignment(boolean writtenByYourself,
+            boolean availableToOthers, boolean writtenAsCourseWork,
+            boolean citingYourSource, boolean implementationRequired) {
+        
+        if(writtenByYourself)
+        	return true;
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("You may certainly use code you wrote yourself: " +
+            RulesOf6005.mayUseCodeInAssignment(true, false, true, true, true));
+    }
+}
